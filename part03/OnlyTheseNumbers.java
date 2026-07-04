2bcf9104ae0fcc6f3e2c8bacc3ac5a74c7d8d300
metrobraj <tobrajendra@gmail.com>
2026-07-05 01:34:53 +0530

    Add files via upload

---
+++ part03/OnlyTheseNumbers.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OnlyTheseNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == -1) {
+                break;
+            }
+
+            numbers.add(number);
+        }
+        System.out.println("From where?");
+        int n1 = scanner.nextInt();
+        System.out.println("To where?");
+        int n2 = scanner.nextInt();
+        for(int i = n1; i<=n2;i++){
+            System.out.println(numbers.get(i));
+        }
+        scanner.close();
+    }
+}
