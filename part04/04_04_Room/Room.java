f499d7abad43bfa871edf2f7af8fbd5783bec119
Brajendra Pandey <tobrajendra@gmail.com>
2026-07-14 11:15:02 +0530

    Additions to part 4

---
+++ part04/04_04_Room/Room.java
@@ -0,0 +1,8 @@
+public class Room {
+    private String code;
+    private int seats;
+    public Room(String classCode, int numberOfSeats){
+        this.seats = numberOfSeats;
+        this.code = classCode;
+    }
+}
