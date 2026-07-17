a5cd98f08aab221a8351ffbee864d85737ea90d0
Brajendra Pandey <toworkbraj@gmail.com>
2026-07-17 12:41:51 +0530

    add

---
+++ part04/04_14_Multiplier/Multiplier.java
@@ -0,0 +1,9 @@
+public class Multiplier {
+    private int num;
+    public Multiplier(int number){
+        this.num = number;
+    }
+    public int multiply(int number){
+        return number*this.num;
+    }
+}
